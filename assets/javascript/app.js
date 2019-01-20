563c9db5d23ad71ca0d3c087ddc71740c19386ce
Peter Tag <peter.h.tag@gmail.com>
2019-01-20 09:55:33 -0800

    Work in progress...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,13 +4,35 @@ window.onload = function() {
         questions : ["Which of the following people is not a member of the K-Pop band BTS?",
                     "What US city has the most microbreweries?",
                     "What city did the baseball Dodgers move from before coming to Los Angeles?",
-                    "Which UC Davis professor is the most adorable and intelligent person on the planet?"],
+                    "Which movie has grossed the most money of all time?"],
         questionChoices : [["Jimin", "RM", "Suga", "Sung Choi", "J-Hope", "Jungkook"],
                         ["Portland", "Boston", "Denver", "St. Louis", "Atlanta", "Sacramento"],
                         ["Boston", "Chicago", "Baltimore", "Pittsburgh", "Cincinatti", "Brooklyn"],
-                        ["Xiaomei Chen", "Xiaomei Chen", "Xiaomei Chen", "Xiaomei Chen", "Xiaomei Chen", "Xiaomei Chen"]],
-        answers : ["Sung Choi", "Portland", "Brooklyn", "Xiaomei Chen"]
-                }
+                        ["Titanic", "Star Wars: The Force Awakens", "Frozen", "Avatar", "Beauty and the Beast", "Forrest Gump"]],
+        answers : {values: ["Sung Choi", "Portland", "Brooklyn", "Avatar"],
+                   GIFs: ["https://media.giphy.com/media/8PduhjwiOxtNjieurJ/giphy.gif",
+                          "https://media.giphy.com/media/jldMwH1mvaRTo5jIBw/giphy.gif",
+                          "https://media.giphy.com/media/gZ7UAPE4SnjJC/giphy.gif",
+                          "https://media.giphy.com/media/AxhxIcTMEMqR2/giphy.gif"
+                        ]}, 
+        nextQuestion : function() {
+            myClock.stop();
+            myChoicesButtonsContainer.empty();
+            currentQuestionIndex++;
+            $("#time-remaining").show();
+            $("#time-remaining-display").show();
+            var initialTimeRemaining = myClock.timeConverter(20);
+            $("#time-remaining-display").text(initialTimeRemaining);    
+            myClock.start();
+            $(this).hide();
+            currentQuestion.show();
+            currentQuestion.text(triviaGame.questions[currentQuestionIndex]);
+            for (var i=0;i<triviaGame.questionChoices[currentQuestionIndex].length;i++) {
+                myChoicesButtonsContainer.append('<button class="btn btn-primary my-choices-buttons">' + triviaGame.questionChoices[currentQuestionIndex][i] + '</button><br>');
+            }  
+        }
+  
+    }
     var currentQuestionIndex = 0;
     var gamesWon = 0;
     var gamesLost = 0;
@@ -24,9 +46,10 @@ window.onload = function() {
     currentQuestion.hide();
 
     $('.start-button').on('click', function(event){
+
         $("#time-remaining").show();
         $("#time-remaining-display").show();
-        var initialTimeRemaining = myClock.timeConverter(60);
+        var initialTimeRemaining = myClock.timeConverter(20);
         $("#time-remaining-display").text(initialTimeRemaining);    
         myClock.start();
         $(this).hide();
@@ -38,7 +61,9 @@ window.onload = function() {
     });
      $(document).on('click', '.my-choices-buttons', function(event) {
         myChoicesButtonsContainer.empty();
-        if ($(this).text() == triviaGame.answers[currentQuestionIndex]) {
+        $("#time-remaining").hide();
+        $("#time-remaining-display").hide();
+        if ($(this).text() == triviaGame.answers.values[currentQuestionIndex]) {
             gamesWon++;
             myChoicesButtonsContainer.append('<pre>Yep!</pre>');
         } else {
@@ -46,23 +71,31 @@ window.onload = function() {
             myChoicesButtonsContainer.append('<pre>Nope!</pre>');
         }
 
-        myChoicesButtonsContainer.append('<pre>The correct answer is ' + triviaGame.answers[currentQuestionIndex] + '</pre>');
-        var answerGIF = $('<img id="my-answer-GIF" src="' + 'https://media.giphy.com/media/8PduhjwiOxtNjieurJ/giphy.gif' + '">');
+        myChoicesButtonsContainer.append('<pre>The correct answer is ' + triviaGame.answers.values[currentQuestionIndex] + '</pre>');
+        var answerGIF = $('<img id="my-answer-GIF" src="' + triviaGame.answers.GIFs[currentQuestionIndex] + '">');
         myChoicesButtonsContainer.append(answerGIF);
-     });
+
+        setTimeout(triviaGame.nextQuestion, 5000);
+      });
+
     var clockRunning = false;
     var myClock = {
         time: 0,
-        remainingTime:  60,
+        remainingTime:  20,
         start: function() {
     
             //  TODO: Use setInterval to start the count here and set the clock to running.
             if (!clockRunning) {
-            intervalId = setInterval(myClock.count, 1000);
-            clockRunning = true;
+                intervalId = setInterval(myClock.count, 1000);
+                clockRunning = true;
             }
             else {
             }
+        },
+        stop: function() {
+            clearInterval(intervalId);
+            clockRunning = false;
+            myClock.remainingTime = 20;
         },
         //_______________________________________
         //
